eead6fa3dd4ced6ae278db24f64011d84cd00f12
wendellucas <wendellucaslrd@gmail.com>
2025-08-06 11:28:24 -0300

    Add files via upload

---
+++ conecta/conDDL.php
@@ -0,0 +1,5 @@
+<?php
+function conectaDDL(){
+    return new mysqli('localhost', 'root', '', 'recanto_doce');
+}
+?>
---
+++ conecta/conecta.php
@@ -0,0 +1,5 @@
+<?php
+function conecta(){
+    return new mysqli('localhost', 'root', '', 'recanto_doce');
+}
+?>
